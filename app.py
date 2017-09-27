ca87ef4677248bec4a0a8c833af8400b6b1e5209
xshi0603 <xshi@stuy.edu>
2017-09-27 13:40:27 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template #importing stuff
 import csv, random 
 
 #old code
 
 jobs = {}
 
-with open('occupations.csv', 'rb') as csvfile:
+with open('occupations.csv', 'rb') as csvfile: #get file and make a dictionary
     reader = csv.reader(csvfile)
     header = next(reader)
     for row in reader:
         jobs[row[0]] = float(row[1])
 
-def randomJob (diction):
-    first = 0
-    second = 0
-    counter = 0
-    targetnum = random.uniform(0, 99.8)
-    print(targetnum)
+def randomJob (diction): #get a random job
+    first = 0 #range bottom
+    second = 0 #range top
+    counter = 0 #how many are u at
+    targetnum = random.uniform(0, 99.8) #float between 0 and 99.8
+    #print(targetnum)
     for item in diction:
-        if counter % 2 == 0:
+        if counter % 2 == 0: #alternates w/ the other one
             second = first + diction[item]
             if (targetnum > first and targetnum < second) :
-                return item
+                return item #return if inbetween the ranges
         else:
             first = second + diction[item]
             if (targetnum < first and targetnum > second) :
-                return item
-        counter += 1
+                return item #return if inbetween the ranges
+        counter += 1 #swap between the two
 #new code
 
 my_app = Flask(__name__)
